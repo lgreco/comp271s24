6a9d19dc27f58574e7af2c5cf832186d550fa818
Leo Irakliotis <lgreco@gmail.com>
2024-06-11 04:21:08 +0000

    week 4 prep

---
+++ Week_04/Main.java
@@ -0,0 +1,8 @@
+package Week_04;
+
+public class Main {
+    public static void main(String[] args) {
+        Undergrad frodo = new Undergrad();
+        System.out.println(frodo);
+    }
+}
---
+++ Week_04/Undergrad.java
@@ -0,0 +1,5 @@
+package Week_04;
+
+public class Undergrad {
+    private String academicMinor;
+}
