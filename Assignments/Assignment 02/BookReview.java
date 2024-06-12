b050dd6257e24e46d2b42278f1198e4288107bf7
Kamaria932 <83671576+Kamaria932@users.noreply.github.com>
2024-06-12 22:11:14 +0000

    assignment 2

--- Assignments/Assignment 02/BookReview.java
+++ Assignments/Assignment 02/BookReview.java
@@ -30,8 +30,34 @@ public class BookReview {
     } // method connecttoBook
 
     public static void main(String[] args) {
-        // https://gutenberg.org/cache/epub/98/pg98.txt is a link
-        // to the text of "Tale of Two Cities" from Project Gutenberg
-        String book = "https://gutenberg.org/cache/epub/98/pg98.txt";
+        // https://gutenberg.org/cach   jfjdfjfjfie/epub/98/pg98.txt is a link
+        // to the text of "Tale of Two Cities" from Project Gutenberg https://www.gutenberg.org/files/98/98-h/98-h.htm
+        String book = "https://learning.oreilly.com/library/view/a-common-sense-guide/9781680508048/f_0013.xhtml";
+        Scanner bookScanner = connectToBook(book);
+
+        if(bookScanner != null){
+            print(CountUniqueWords);
+        }
     } // method main
-} // class BookReview
+    public static void (CountUniqueWords){
+        boolean[] array = new boolean[words.length];
+        int j, i = 0;
+        int count = 0;
+        for (i = 0; i < words.length; i++) {
+                count++;
+                for (j = i + 1; j < words.length; j++) {
+                    if (words[j].compareTo(words[i]) == 0) {
+                        array[j] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args)
+    {
+        int count = CountUniqueWords(str);
+        System.out.println(count);
+    }
