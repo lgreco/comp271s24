9af2add46e3b14a1a32047eb6bd434b14bdd1256
Leo Irakliotis <lgreco@gmail.com>
2024-06-17 22:55:51 +0000

    train stations

--- Week_05/Station.java
+++ Week_05/Station.java
@@ -1,12 +1,21 @@
+/**
+ * Object that simulates a simple train station. These objects are used by
+ * class TrainLine to simulate a train route.
+ */
 public class Station {
 
+    /** The name of the train station */
     private String name; 
+    /** The station immediately after */
     private Station next;
 
     /**
-     * To place a train station in a train line, we go to the 
-     * beginning of the train line, follow the line to its last
-     * station, and thence add thew station.
+     * A simple constructor to instantiate a Station object with a given name
+     * but unknown (null) next station.
+     * @param name
      */
-
-}
+    public Station(String name) {
+        this.name = name;
+        this.next = null;
+    } // simple constructor
+} // class Station
