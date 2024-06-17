1131647751f7a5161b6a902cf226798577d2a062
Leo Irakliotis <lgreco@gmail.com>
2024-06-17 16:24:01 +0000

    week 5 setup

---
+++ Week_05/Station.java
@@ -0,0 +1,12 @@
+public class Station {
+
+    private String name; 
+    private Station next;
+
+    /**
+     * To place a train station in a train line, we go to the 
+     * beginning of the train line, follow the line to its last
+     * station, and thence add thew station.
+     */
+
+}
