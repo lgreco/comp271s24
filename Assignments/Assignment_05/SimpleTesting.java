78dba03961b2ae41229a8c977c964dce527f5651
roshnimishra1 <rmishra@luc.edu>
2024-07-03 16:44:59 +0000

    A5 SimpleTesting

--- Assignments/Assignment_05/SimpleTesting.java
+++ Assignments/Assignment_05/SimpleTesting.java
@@ -22,5 +22,31 @@ public class SimpleTesting {
         // Test non existent
         boolean nonExisting = (redLineSB.indexOf(NON_EXISTENT) == -1);
         System.out.printf("\n\nSequence test: %s", (nonExisting ? PASS : FAIL));
+
+        // another TrainLine and adds new stations.
+        TrainLine continueRedLineSB = new TrainLine();
+        String[] continueRedLine = {"Granville", "Thorndale", "Bryn Mawr"};
+        for (String name : continueRedLine){
+            continueRedLineSB.addStation(name);
+        }
+        // appends the new to the trainline
+        redLineSB.append(continueRedLineSB);
+
+        String[] StationsTogether = { "Howard", "Jarvis", "Morse",
+                "Loyola", "Granville", "Thorndale", "Bryn Mawr" };
+            boolean appendSuccess = true;
+            for (int i = 0; i < StationsTogether.length; i++) {
+                appendSuccess = appendSuccess && (redLineSB.indexOf(StationsTogether[i] == i);
+
+                }
+                System.out.printf("\n\nAppend test: %s", (appendSuccess ? PASS : FAIL));
+
+                //outputs the final train line 
+                System.out.printf("\n\nFinal train line:  %s", redLineSB.toString());
+            }
+        }
+
+
+
     }
 }
